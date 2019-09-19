1ed57d20d60f5d2e68d4d002135773982e7f688e
Rodrigo Mendes <rodrigo.mendes15@hotmail.com>
2019-09-19 10:53:46 -0300

    Primeiro MVC - Departament

---
+++ Controllers/DepartamentsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProjetoWebMVC.Models;
+
+namespace ProjetoWebMVC.Controllers
+{
+    public class DepartamentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departament> list = new List<Departament>();
+            list.Add(new Departament { Id = 1, Nome = "Eletrônicos" });
+            list.Add(new Departament { Id = 2, Nome = "Moda" });
+
+            return View(list);
+        }
+    }
+}
---
+++ Models/Departament.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjetoWebMVC.Models
+{
+    public class Departament
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+    }
+}
