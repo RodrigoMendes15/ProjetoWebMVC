147f786ecc07f55fbd17df224d7882c0abd77d32
Rodrigo Mendes <rodrigo.mendes15@hotmail.com>
2019-10-02 17:34:05 -0300

    Campo de selecao do departamento ao criar novo vendedor

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ProjetoWebMVC.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using ProjetoWebMVC.Models;
 using ProjetoWebMVC.Services;
@@ -11,10 +12,12 @@ namespace ProjetoWebMVC.Controllers
     public class SellersController : Controller
     {
         private readonly SellerService _sellerService;
+        private readonly DepartmentService _departmentService;
 
-        public SellersController(SellerService sellerService)
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
             _sellerService = sellerService;
+            _departmentService = departmentService;
         }
 
         public IActionResult Index()
@@ -25,7 +28,9 @@ namespace ProjetoWebMVC.Controllers
 
         public IActionResult Create()
         {
-            return View();
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel { Departments = departments };
+            return View(viewModel);
         }
 
         [HttpPost]
---
+++ Services/DepartmentService.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using ProjetoWebMVC.Models;
+using System.Collections.Generic;
+
+namespace ProjetoWebMVC.Services
+{
+    public class DepartmentService
+    {
+        private readonly ProjetoWebMVCContext _context;
+
+        public DepartmentService(ProjetoWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Nome).ToList();
+        }
+    }
+}
