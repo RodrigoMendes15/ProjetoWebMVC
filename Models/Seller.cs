794f7b951a1faf11061c1807a18254e9cd276161
Rodrigo Mendes <rodrigo.mendes15@hotmail.com>
2019-10-02 19:41:16 -0300

    Correcao dos campos de data, salario e e-mail

--- Models/Seller.cs
+++ Models/Seller.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProjetoWebMVC.Models
 {
@@ -8,8 +9,18 @@ namespace ProjetoWebMVC.Models
     {
         public int Id { get; set; }
         public string Nome { get; set; }
+
+        [Display(Name = "Endereço Eletrônico")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        [Display(Name = "Data de Nascimento")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime DataNascimento { get; set; }
+
+        [Display(Name = "Salário Base")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double SalarioBase { get; set; }
         public Department Departamento { get; set; }
         public int DepartmentId { get; set; }
