81dbf89c7c39aeb83da5c2a094497352b710b114
Rodrigo Mendes <rodrigo.mendes15@hotmail.com>
2019-10-02 20:05:15 -0300

    validacoes do formulario de vendedor

--- Models/Seller.cs
+++ Models/Seller.cs
@@ -8,19 +8,27 @@ namespace ProjetoWebMVC.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} obrigarório!")]
+        [StringLength(70, MinimumLength = 3, ErrorMessage = "O campo {0} deve possuir entre {2} e {1} caracteres!")]
         public string Nome { get; set; }
 
         [Display(Name = "Endereço Eletrônico")]
         [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "{0} obrigarório!")]
+        [EmailAddress(ErrorMessage = "{0} obrigatório!")]
         public string Email { get; set; }
 
         [Display(Name = "Data de Nascimento")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [Required(ErrorMessage = "{0} obrigarório!")]
         public DateTime DataNascimento { get; set; }
 
         [Display(Name = "Salário Base")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
+        [Required(ErrorMessage = "{0} obrigarório!")]
+        [Range(100.0, 50000.0, ErrorMessage = "O {0} deve estar entre {1} a {2}")]
         public double SalarioBase { get; set; }
         public Department Departamento { get; set; }
         public int DepartmentId { get; set; }
