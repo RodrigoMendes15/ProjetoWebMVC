cdbbe2ed00eaaa8ccb5b66747b0b88b71d588295
Rodrigo Mendes <rodrigo.mendes15@hotmail.com>
2019-10-03 12:21:48 -0300

    Implantado a consulta agrupada

--- Models/SalesRecord.cs
+++ Models/SalesRecord.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 using ProjetoWebMVC.Models.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProjetoWebMVC.Models
 {
     public class SalesRecord
     {
         public int Id { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Data { get; set; }
+
+        [DisplayFormat(DataFormatString ="{0:F2}")]
         public double Valor { get; set; }
+
         public SaleStatus Status { get; set; }
+
         public Seller Vendas { get; set; }
 
         public SalesRecord()
