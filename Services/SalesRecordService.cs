cf3e5405d5d61121b0045996221d9e1149060273
Rodrigo Mendes <rodrigo.mendes15@hotmail.com>
2019-10-03 11:52:57 -0300

    Iniciado o processo dos campos de busca, busca simples concluida

---
+++ Services/SalesRecordService.cs
@@ -0,0 +1,56 @@
+﻿using ProjetoWebMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjetoWebMVC.Services
+{
+    public class SalesRecordService
+    {
+        private readonly ProjetoWebMVCContext _context;
+
+        public SalesRecordService(ProjetoWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Data >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Data <= maxDate.Value);
+            }
+            return await result
+                .Include(x => x.Vendas)
+                .Include(x => x.Vendas.Departamento)
+                .OrderByDescending(x => x.Data)
+                .ToListAsync();
+        }
+
+        public async Task<List<IGrouping<Department, SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Data >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Data <= maxDate.Value);
+            }
+            return await result
+                .Include(x => x.Vendas)
+                .Include(x => x.Vendas.Departamento)
+                .OrderByDescending(x => x.Data)
+                .GroupBy(x => x.Vendas.Departamento)
+                .ToListAsync();
+        }
+    }
+}
